9453c838968b692608c8cb3f6a3bbb22d8adddb4
Anton <137609245+Ddonec@users.noreply.github.com>
2024-02-24 01:05:06 +0300

    feat: refacror code; add buttons for change level

--- Flappy-Bird-Game-main/script.js
+++ Flappy-Bird-Game-main/script.js
@@ -1,12 +1,17 @@
 // let move_speed = 10;
 // const fixedMoveSpeed = 5;
-let gravity = 0;
 let bird = document.querySelector(".bird");
 let img = document.getElementById("bird-1");
 // let sound_point = new Audio("sounds effect/point.mp3");
 // let sound_die = new Audio("sounds effect/die.mp3");
 
-let move_speed = 0.001; // Скорость будет относительной ширине экрана
+let gravity = 0.35;
+let pipe_gap = 55; // Объявляем переменную pipe_gap здесь
+let move_speed = 0.005; // Скорость будет относительной ширине экрана
+let firstRoundProcent = "%";
+let ferstRoundVh = "vh";
+let coffForTrain = 1;
+
 let fixedMoveSpeed = move_speed * window.innerWidth; // Рассчитываем фиксированную скорость
 
 let bird_props = bird.getBoundingClientRect();
@@ -22,12 +27,7 @@ message.classList.add("messageStyle");
 let isJumping = false;
 let isGameOver = false; // добавляем переменную для отслеживания статуса игры
 
-let stoneImages = [
-   "KAMEN_4.png",
-   "KAMEN_5.png",
-   "KAMEN_6.png",
-   "KAMEN_7.png"
-];
+let stoneImages = ["KAMEN_4.png", "KAMEN_5.png", "KAMEN_6.png", "KAMEN_7.png"];
 
 document.body.addEventListener("touchstart", function (e) {
    console.log("Ты тапнул по экрану!");
@@ -36,14 +36,6 @@ document.body.addEventListener("touchstart", function (e) {
       document.querySelectorAll(".pipe_sprite").forEach((elem) => {
          elem.remove();
       });
-      img.style.display = "block";
-      bird.style.top = "20vh";
-      game_state = "Play";
-      message.innerHTML = "";
-      score_title.innerHTML = "Score : ";
-      score_val.innerHTML = "0";
-      message.classList.remove("messageStyle");
-      play();
    }
 
    if (e.touches.length > 0) {
@@ -52,7 +44,7 @@ document.body.addEventListener("touchstart", function (e) {
    }
 });
 
-document.addEventListener("touchend", (e) => {
+document.addEventListener("touchend", () => {
    img.src = "images/DRAKON_LITTLE_1.png";
 });
 
@@ -65,14 +57,6 @@ function handleKeyPress(e) {
          document.querySelectorAll(".pipe_sprite").forEach((e) => {
             e.remove();
          });
-         img.style.display = "block";
-         bird.style.top = "20vh";
-         game_state = "Play";
-         message.innerHTML = "";
-         score_title.innerHTML = "Score : ";
-         score_val.innerHTML = "0";
-         message.classList.remove("messageStyle");
-         play();
       }
 
       if (e.key == "ArrowUp" || e.key == " ") {
@@ -88,13 +72,10 @@ function handleKeyPress(e) {
 }
 
 function play() {
-   // isGameOver = false;
-   let pipe_separation = 0; // Объявляем переменную pipe_separation здесь
-   let pipe_gap = 65; // Объявляем переменную pipe_gap здесь
-
+   isGameOver = false;
 
    function move() {
-      // if (game_state != "Play") return;
+      if (game_state != "Play") return;
 
       // Устанавливаем фиксированную скорость движения препятствий
 
@@ -106,21 +87,20 @@ function play() {
          if (pipe_sprite_props.right <= 0) {
             element.remove();
          } else {
-            // if (
-            //    bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width &&
-            //    bird_props.left + bird_props.width > pipe_sprite_props.left &&
-            //    bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height &&
-            //    bird_props.top + bird_props.height > pipe_sprite_props.top
-            // ) {
-            //    game_state = "End";
-            //    message.innerHTML = "Game Over".fontcolor("red") + "<br>Press Enter or Space To Restart";
-            //    message.classList.add("messageStyle");
-            //    isGameOver = true; // Устанавливаем переменную состояния игры в true
-            //    //    sound_die.play();
-            //    return;
-            // }
-            //  else
-            {
+            if (
+               bird_props.left < pipe_sprite_props.left + pipe_sprite_props.width &&
+               bird_props.left + bird_props.width > pipe_sprite_props.left &&
+               bird_props.top < pipe_sprite_props.top + pipe_sprite_props.height &&
+               bird_props.top + bird_props.height > pipe_sprite_props.top
+            ) {
+               game_state = "End";
+               message.innerHTML = "Game Over".fontcolor("red") + "<br>Press Enter or Space To Restart" + '<button onclick="goToMenu()">Menu</button>';
+               message.classList.add("messageStyle");
+               isGameOver = true; // Устанавливаем переменную состояния игры в true
+               //    sound_die.play();
+
+               return;
+            } else {
                if (pipe_sprite_props.right < bird_props.left && pipe_sprite_props.right + fixedMoveSpeed >= bird_props.left && element.increase_score == "1") {
                   score_val.innerHTML = +score_val.innerHTML + 1;
                   //   sound_point.play();
@@ -134,55 +114,130 @@ function play() {
    }
    requestAnimationFrame(move);
 
+   let bird_dy = 0;
+   function apply_gravity() {
+      if (game_state != "Play") return;
+      bird_dy = bird_dy + gravity;
+
+      if (isJumping) {
+         img.src = "images/DRAKON_LITTLE_2.png";
+         bird_dy = -6;
+         isJumping = false;
+      }
+
+      if (bird_props.top <= 0 || bird_props.bottom >= background.bottom) {
+         game_state = "End";
+         message.style.left = "28vw";
+         window.location.reload();
+         message.classList.remove("messageStyle");
+         return;
+      }
+
+      bird.style.top = bird_props.top + bird_dy + "px";
+      bird_props = bird.getBoundingClientRect();
+      requestAnimationFrame(apply_gravity);
+   }
+   requestAnimationFrame(apply_gravity);
+
    function create_pipe() {
       if (game_state != "Play") return;
-  
-      let pipeInterval = setInterval(() => {
-          if (pipe_separation > 200) {
-              pipe_separation = 0;
-  
-              let pipe_posi = Math.floor(Math.random() * 43) + 8;
-  
-              // Создаем контейнеры для труб
-              let pipe_sprite_inv = document.createElement("div");
-              pipe_sprite_inv.className = "pipe_sprite";
-              pipe_sprite_inv.style.top = pipe_posi - 20 + "vh";
-              pipe_sprite_inv.style.left = "100vw";
-              document.body.appendChild(pipe_sprite_inv);
-  
-              let pipe_sprite = document.createElement("div");
-              pipe_sprite.className = "pipe_sprite";
-              pipe_sprite.style.top = pipe_posi + pipe_gap + "vh";
-              pipe_sprite.style.left = "100vw";
-              pipe_sprite.increase_score = "1";
-              document.body.appendChild(pipe_sprite);
-  
-              // Создаем изображение внутри каждого контейнера трубы
-              let pipe_image_inv = document.createElement("img");
-              let randomStoneImage = stoneImages[Math.floor(Math.random() * stoneImages.length)];
-              pipe_image_inv.src = `images/stones/${randomStoneImage}`;
-              pipe_image_inv.style.height = "120%";
-              pipe_image_inv.style.position = "relative";
-              pipe_image_inv.style.top = "-10%";
-              pipe_sprite_inv.appendChild(pipe_image_inv);
-  
-              let pipe_image = document.createElement("img");
-              let randomStoneImage2 = stoneImages[Math.floor(Math.random() * stoneImages.length)];
-              pipe_image.src = `images/stones/${randomStoneImage2}`;
-              pipe_image.style.height = "120%";
-              pipe_image.style.position = "relative";
-              pipe_image.style.bottom = "-10%";
-              pipe_sprite.appendChild(pipe_image);
-          }
-          pipe_separation++;
-  
-          if (isGameOver) {
-              clearInterval(pipeInterval);
-          }
-      }, 40);
-  }
-   
+      let pipe_separation = 0; // Объявляем переменную pipe_separation здесь
 
-   
+      let pipeInterval = setInterval(() => {
+         if (pipe_separation > 300) {
+            pipe_separation = 0;
+
+            let pipe_posi = Math.floor(Math.random() * (25 - 10 + 1)) + 10;
+
+            // Создаем контейнеры для труб
+            let pipe_sprite_inv = document.createElement("div");
+            pipe_sprite_inv.className = "pipe_sprite";
+            pipe_sprite_inv.style.top = pipe_posi * coffForTrain - 70 + ferstRoundVh;
+            pipe_sprite_inv.style.left = "100vw";
+            document.body.appendChild(pipe_sprite_inv);
+
+            let pipe_sprite = document.createElement("div");
+            pipe_sprite.className = "pipe_sprite";
+            pipe_sprite.style.top = pipe_posi + pipe_gap + "vh";
+            pipe_sprite.style.left = "100vw";
+            pipe_sprite.increase_score = "1";
+            document.body.appendChild(pipe_sprite);
+
+            // Создаем изображение внутри каждого контейнера трубы
+            let pipe_image_inv = document.createElement("img");
+            let randomStoneImage = stoneImages[Math.floor(Math.random() * stoneImages.length)];
+            pipe_image_inv.src = `images/stones/${randomStoneImage}`;
+            pipe_image_inv.style.height = "120%";
+            pipe_image_inv.style.position = "relative";
+            pipe_image_inv.style.top = "-10%";
+            pipe_sprite_inv.appendChild(pipe_image_inv);
+
+            let pipe_image = document.createElement("img");
+            let randomStoneImage2 = stoneImages[Math.floor(Math.random() * stoneImages.length)];
+            pipe_image.src = `images/stones/${randomStoneImage2}`;
+            pipe_image.style.height = "120%";
+            pipe_image.style.position = "relative";
+            pipe_image.style.bottom = "-10%";
+            pipe_sprite.appendChild(pipe_image);
+         }
+         pipe_separation++;
+
+         if (isGameOver) {
+            clearInterval(pipeInterval);
+         }
+      }, 10);
+   }
+
    requestAnimationFrame(create_pipe);
+}
+
+function StartRound() {
+   img.style.display = "block";
+   bird.style.top = "20vh";
+   game_state = "Play";
+   message.innerHTML = "";
+   score_title.innerHTML = "Score : ";
+   score_val.innerHTML = "0";
+   message.classList.remove("messageStyle");
+   play();
+}
+
+// Получаем ссылки на кнопки
+const trainingBtn = document.querySelector("button:nth-of-type(1)");
+const easyBtn = document.querySelector("button:nth-of-type(2)");
+const normalBtn = document.querySelector("button:nth-of-type(3)");
+const hardBtn = document.querySelector("button:nth-of-type(4)");
+
+// Добавляем обработчики событий для каждой кнопки
+trainingBtn.addEventListener("click", () => {
+   move_speed = 10;
+   pipe_gap = 50;
+   StartRound();
+});
+
+easyBtn.addEventListener("click", () => {
+   gravity = 0.3;
+   move_speed = 0.005;
+   pipe_gap = 60;
+   StartRound();
+});
+
+normalBtn.addEventListener("click", () => {
+   gravity = 0.3;
+   move_speed = 0.005;
+   pipe_gap = 70;
+   ferstRoundVh = "%";
+   coffForTrain = 0;
+   StartRound();
+});
+
+hardBtn.addEventListener("click", () => {
+   gravity = 0.3;
+   move_speed = 0.009;
+   pipe_gap = 40;
+   StartRound();
+});
+
+function goToMenu() {
+   message.classList.remove("none");
 }
